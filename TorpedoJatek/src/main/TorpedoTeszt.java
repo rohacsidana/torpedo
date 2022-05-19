6383219612e4ff59b95ce1ae144a8bab0b019b83
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 14:51:25 +0200

    tesztLoves feltoltese

--- TorpedoJatek/src/main/TorpedoTeszt.java
+++ TorpedoJatek/src/main/TorpedoTeszt.java
@@ -3,10 +3,13 @@ package main;
 public class TorpedoTeszt {
 
     public static void main(String[] args) {
-
+        tesztLoves(4);
     }
 
-    public String tesztLoves(int poz) {
+    public static String tesztLoves(int poz) {
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(poz);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
 
