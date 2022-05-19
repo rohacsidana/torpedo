6a38c8ccf579e1e414096a5445cab293e6f3c068
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 14:46:06 +0200

    Hajo class létrehozása, +adattag, +metódus

---
+++ TorpedoJatek/src/main/Hajo.java
@@ -0,0 +1,10 @@
+package main;
+
+
+public class Hajo {
+    private int[] pozicio = new int[3];
+    
+    public String talalat(int poz){
+        return "";
+    }
+}
--- TorpedoJatek/src/main/TorpedoTeszt.java
+++ TorpedoJatek/src/main/TorpedoTeszt.java
@@ -1,11 +1,13 @@
-
 package main;
-
 
 public class TorpedoTeszt {
 
     public static void main(String[] args) {
-        
+
+    }
+
+    public String tesztLoves(int poz) {
+        return "";
     }
 
 }
